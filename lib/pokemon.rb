f3411bc3972514e8a98b61624e1ec3a510653cae
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-30 15:01:00 -0500

    done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+attr_accessor :id, :name, :type, :db 
+
+    def initialize(id:, name:, type:, db:)
+        @id= id
+        @name=name 
+        @type=type
+        @db=db
+    end 
+
+    def self.save(name, type, db) 
+        db.execute("INSERT INTO pokemon (name, type) VALUES ('#{name}', '#{type}')")
+        
+        # (name, type, db) VALUES (name, type, db)")
+    end 
+
+    def self.find(id, db)
+        db.execute("SELECT pokemon.id FROM pokemon WHERE id = ('#{id}')")
+
+    else 
+        name = db.execute("SELECT name FROM pokemon WHERE id= '#{id}'")
+        type = db.execute("SELECT type FROM pokemon WHERE id= '#{id}'")
+        self.save(name, type, db)
+    end 
+
 end
